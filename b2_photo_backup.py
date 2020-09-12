06740feb6f42708ff8c6d9ffe2dddaab35065be3
handleman <pavel.handleman@gmail.com>
2020-09-12 18:25:51 +0300

    deploy completed

--- b2_photo_backup.py
+++ b2_photo_backup.py
@@ -3,6 +3,8 @@ import os
 import base64
 import json
 import urllib.request
+import hashlib
+import urllib.parse
 
 from urllib.request import Request, urlopen
 
@@ -32,7 +34,6 @@ def init_argparse() -> argparse.ArgumentParser:
 
 
 def b2_authorize(applicationKeyId, applicationKeyValue):
-    # todo: B2 app id and app key should be passed ass argument
     id_and_key = f'{applicationKeyId}:{applicationKeyValue}'
     id_and_key_base64 = base64.b64encode(
         id_and_key.encode('utf-8')).decode('utf-8')
@@ -43,29 +44,56 @@ def b2_authorize(applicationKeyId, applicationKeyValue):
     request = Request(b2_auth_url, headers=headers)
     with urlopen(request) as response:
         auth = json.loads(response.read())
+    response.close()
     return auth
 
 
-def b2_upload_url(filePath, apiUrl, authToken, bucketId):
+def b2_get_upload_url(filePath, apiUrl, authToken, bucketId):
     b2_get_upload_url = f'{apiUrl}/b2api/v2/b2_get_upload_url'
     get_url_body = {'bucketId': bucketId}
     get_url_headers = {'Authorization': authToken}
     request = Request(b2_get_upload_url, data=json.dumps(get_url_body).encode('utf-8'), headers=get_url_headers)
     with urlopen(request) as response:
         uploadData = json.loads(response.read())
-    pprint(uploadData)
+    response.close()
+    return uploadData
 
-def applyForFile(filePath, callback):
+def b2_upload_file_callback(uploadUrl, authToken):
+    def _callback(filePathName):
+        content_type = 'b2/x-auto'
+        file_path_name_encoded = urllib.parse.quote(filePathName)
+        with open(filePathName, 'br') as file:
+            file_data = file.read()
+        file.close()
+        file_hash = hashlib.sha1(file_data).hexdigest()
+
+        headers = {
+            'Authorization': authToken,
+            'X-Bz-File-Name': file_path_name_encoded,
+            'Content-Type': content_type,
+            'X-Bz-Content-Sha1': file_hash
+        }
+
+        request = Request(uploadUrl, data=file_data, headers=headers)
+
+        with urlopen(request) as response:
+            upload_info = json.loads(response.read())
+        response.close()
+        file_name = upload_info['fileName']
+        print(f'[ Uploaded Successfully ]: {file_name}')
+
+    return _callback
+
+
+def applyForFile(filesPath, callback):
     excludes = ['.DS_Store', '.Trashes', '.fseventsd', '.Spotlight-V100']
 
-    for root, directories, files in os.walk(filePath):
+    for root, directories, files in os.walk(filesPath):
         for name in files:
             if name not in excludes:
-                pprint(os.path.join(root, name))
+                full_name = os.path.join(root, name)
+                callback(full_name)
 
-
-def _callback(filePath):
-    pprint(filePath)
 
 
 def main() -> None:
@@ -76,10 +104,14 @@ def main() -> None:
     keyId = args["key_id"]
     keyValue = args["app_key"]
 
-    applyForFile(directory, _callback)
     auth = b2_authorize(keyId, keyValue)
 
-    uploadSettings = b2_upload_url('tempfile', auth['apiUrl'], auth['authorizationToken'], auth['allowed']['bucketId'])
+    uploadSettings = b2_get_upload_url('tempfile', auth['apiUrl'], auth['authorizationToken'], auth['allowed']['bucketId'])
+
+    applyForFile(directory, b2_upload_file_callback(uploadSettings['uploadUrl'], uploadSettings['authorizationToken']))
+
+    print('[ All files were successfully uploaded ]!')
+
 
 if __name__ == "__main__":
     main()
